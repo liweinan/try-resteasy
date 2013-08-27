e91c450899ffbf2a171ccc11ffb0377128ef999e
Weinan Li <weli@redhat.com>
2013-08-27 17:31:19 +0800

    skeleton

--- src/main/java/io/app/MyApplication.java
+++ src/main/java/io/app/MyApplication.java
@@ -1,11 +1,12 @@
 package io.app;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 /**
  * @author <a href="mailto:l.weinan@gmail.com">Weinan Li</a>
  */
 
 @ApplicationPath("/resteasy")
-public class MyApplication {
+public class MyApplication extends Application {
 }
