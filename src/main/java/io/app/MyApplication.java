37c6377db779d305553460a91220e3439db76dd3
Weinan Li <weli@redhat.com>
2013-08-27 17:24:43 +0800

    cleanup

---
+++ src/main/java/io/app/MyApplication.java
@@ -0,0 +1,11 @@
+package io.app;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * @author <a href="mailto:l.weinan@gmail.com">Weinan Li</a>
+ */
+
+@ApplicationPath("/resteasy")
+public class MyApplication {
+}
