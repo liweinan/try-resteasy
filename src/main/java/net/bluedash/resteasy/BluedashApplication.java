97716d4603a732e26da279314f20417850091c4f
Weinan Li <weli@redhat.com>
2013-08-27 17:18:21 +0800

    cleanup

---
+++ src/main/java/net/bluedash/resteasy/BluedashApplication.java
@@ -0,0 +1,14 @@
+package net.bluedash.resteasy;
+
+import javax.ws.rs.core.Application;
+
+/**
+ * 11 01 2012
+ *
+ * @author <a href="mailto:l.weinan@gmail.com">Weinan Li</a>
+ */
+public class BluedashApplication extends Application {
+
+
+
+}
